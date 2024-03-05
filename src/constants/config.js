02e9d4c2ef26ccfb024bb998dddb0cc29c251ddf
Võ Anh Khôi <141430448+UniKing1s@users.noreply.github.com>
2024-03-05 15:27:36 +0700

    Update config.js

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,3 +1,4 @@
 export const booksAPI_URL =
-  "https://quanlythuvienbackend.khoivokhongdinh.repl.co";
+"https://a10b31f6-479e-4c44-a55e-71d85a89bdd8-00-35afhr3che38n.pike.repl.co";
+//"https://quanlythuvienbackend.khoivokhongdinh.repl.co";
 // export const booksAPI_URL = "http://localhost:5000";
